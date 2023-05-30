2f77627b9b0bf01e2715981de171a7f0eec2d517
Janosense <janosense@gmail.com>
2023-05-31 02:07:53 +0300

    Fixed bugs

--- wp-content/themes/player-key/index.php
+++ wp-content/themes/player-key/index.php
@@ -10,6 +10,9 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="/wp-content/themes/player-key/dist/css/main.css">
+    <script>
+		const wp = <?php echo json_encode( [ 'nonce' => wp_create_nonce( 'app' ) ] );?>
+    </script>
 </head>
 <body>
 <div id="app"></div>
