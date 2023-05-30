815d0873da0fd88cc9eb12e2ff14a327daaa7350
Janosense <janosense@gmail.com>
2023-05-31 02:18:47 +0300

    Fixed bugs

--- wp-content/themes/player-key/src/api/authentication.js
+++ wp-content/themes/player-key/src/api/authentication.js
@@ -3,19 +3,35 @@ import axios from 'axios'
 axios.defaults.baseURL = 'https://playerkeyid.wpengine.com/wp-json/pki/v1'
 
 const registerUser = (data) => {
-    return axios.post('/users/register', data)
+    return axios.post('/users/register', data, {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
 }
 
 const loginUser = (data) => {
-    return axios.post('/users/login', data)
+    return axios.post('/users/login', data, {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
 }
 
 const checkUser = (data) => {
-    return axios.post('/users/check', data);
+    return axios.post('/users/check', data, {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
 }
 
 const logoutUser = (data) => {
-    return axios.post('/users/logout', data);
+    return axios.post('/users/logout', data, {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
 }
 
 export default {
--- wp-content/themes/player-key/src/api/entities.js
+++ wp-content/themes/player-key/src/api/entities.js
@@ -3,19 +3,35 @@ import axios from 'axios'
 axios.defaults.baseURL = 'https://playerkeyid.wpengine.com/wp-json/pki/v1'
 
 const getEntitles = (data) => {
-    return axios.post(`/entities/${data.entityType}`, data)
+    return axios.post(`/entities/${data.entityType}`, data, {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
 }
 
 const createEntity = (data) => {
-    return axios.post(`/entities/create-${data.entityType}`, data)
+    return axios.post(`/entities/create-${data.entityType}`, data, {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
 }
 
 const removeEntity = (data) => {
-    return axios.post(`/entities/remove-${data.entityType}`, data)
+    return axios.post(`/entities/remove-${data.entityType}`, data, {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
 }
 
 const editEntity = (data) => {
-    return axios.post(`/entities/edit-${data.entityType}`, data)
+    return axios.post(`/entities/edit-${data.entityType}`, data, {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
 }
 
 export default {
