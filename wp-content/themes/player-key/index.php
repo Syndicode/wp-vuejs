7eb5fe638577358947d02c7066e9c69e6adbc041
Janosense <janosense@gmail.com>
2023-06-10 11:35:05 +0300

    Changed favicon

--- wp-content/themes/player-key/index.php
+++ wp-content/themes/player-key/index.php
@@ -22,7 +22,6 @@
     <link rel="icon" type="image/png" sizes="32x32" href="/wp-content/themes/player-key/src/assets/favicon/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/wp-content/themes/player-key/src/assets/favicon/favicon-16x16.png">
     <link rel="manifest" href="/wp-content/themes/player-key/src/assets/favicon/site.webmanifest">
-    <link rel="mask-icon" href="/wp-content/themes/player-key/src/assets/favicon/safari-pinned-tab.svg" color="#5bbad5">
     <meta name="msapplication-TileColor" content="#da532c">
     <meta name="theme-color" content="#ffffff">
     <!-- Favicon end -->
