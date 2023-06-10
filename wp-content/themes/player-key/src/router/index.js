a06c9f122877b665d705310fcefd206b07876263
Janosense <janosense@gmail.com>
2023-06-10 11:30:53 +0300

    Added page titles, favicon

--- wp-content/themes/player-key/src/router/index.js
+++ wp-content/themes/player-key/src/router/index.js
@@ -9,54 +9,81 @@ import CompleteAthleteView from '../views/CompleteAthleteView.vue'
 import PasswordResetView from '../views/PasswordResetView.vue'
 import store from '../store/index.js'
 
-
+const SITE_TITLE = 'Player Key ID';
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         {
             path: '/',
             name: 'home',
-            component: HomeView
+            component: HomeView,
+            meta: {
+                title: SITE_TITLE
+            },
         },
         {
             path: '/sign-up',
             name: 'sign-up',
-            component: SignUpView
+            component: SignUpView,
+            meta: {
+                title: `Sign up - ${SITE_TITLE}`
+            },
         },
         {
             path: '/password-reset',
             name: 'password-reset',
-            component: PasswordResetView
+            component: PasswordResetView,
+            meta: {
+                title: `Password reset - ${SITE_TITLE}`
+            },
         },
         {
             path: '/sign-up/:role',
             name: 'sign-up-role',
-            component: SignUpRoleView
+            component: SignUpRoleView,
+            meta: {
+                title: `Sign up - ${SITE_TITLE}`
+            },
         },
         {
             path: '/sign-in',
             name: 'sign-in',
-            component: SignInView
+            component: SignInView,
+            meta: {
+                title: `Sign in - ${SITE_TITLE}`
+            },
         },
         {
             path: '/board',
             name: 'board',
-            component: BoardView
+            component: BoardView,
+            meta: {
+                title: `Board - ${SITE_TITLE}`
+            },
         },
         {
             path: '/board/:entity',
             name: 'board-entity',
-            component: BoardView
+            component: BoardView,
+            meta: {
+                title: `Board - ${SITE_TITLE}`
+            },
         },
         {
             path: '/complete/parent',
             name: 'complete-parent',
-            component: CompleteParentView
+            component: CompleteParentView,
+            meta: {
+                title: `Complete Parent information - ${SITE_TITLE}`
+            },
         },
         {
             path: '/complete/athlete',
             name: 'complete-athlete',
-            component: CompleteAthleteView
+            component: CompleteAthleteView,
+            meta: {
+                title: `Complete Athlete information - ${SITE_TITLE}`
+            },
         },
     ]
 });
@@ -68,5 +95,9 @@ router.beforeEach(async (to, from) => {
         });
     }
 })
+
+router.afterEach((to, from) => {
+    document.title = to.meta.title || SITE_TITLE
+});
 
 export default router
