93aa1f66c6a3de52402277863259d7caf886f38d
Janosense <janosense@gmail.com>
2023-06-10 11:46:52 +0300

    Changed favicon

--- wp-content/themes/player-key/index.php
+++ wp-content/themes/player-key/index.php
@@ -18,12 +18,11 @@
     <!-- Main styles end -->
 
     <!-- Favicon start -->
-    <link rel="apple-touch-icon" sizes="180x180" href="/wp-content/themes/player-key/src/assets/favicon/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/wp-content/themes/player-key/src/assets/favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/wp-content/themes/player-key/src/assets/favicon/favicon-16x16.png">
-    <link rel="manifest" href="/wp-content/themes/player-key/src/assets/favicon/site.webmanifest">
-    <meta name="msapplication-TileColor" content="#da532c">
-    <meta name="theme-color" content="#ffffff">
+    <link rel="apple-touch-icon" sizes="180x180" href="/wp-content/themes/player-key/src/assets/favicon/apple-touch-iconi.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/wp-content/themes/player-key/src/assets/favicon/favicon-32x32i.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/wp-content/themes/player-key/src/assets/favicon/favicon-16x16i.png">
+    <link rel="mask-icon" href="/wp-content/themes/player-key/src/assets/favicon/safari-pinned-tab.svg" color="#5bbad5">
+    <link rel="manifest" href="/wp-content/themes/player-key/src/assets/favicon/sitei.webmanifest">
     <!-- Favicon end -->
 </head>
 <body>
